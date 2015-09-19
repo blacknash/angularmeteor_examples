ca1a9b2cbbd435b9870137414758ea57bca10bc3
Josue G <josue.ggh@gmail.com>
2015-09-19 00:32:16 -0500

    Agregando conexion a mongo

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
+People = new Mongo.Collection("people"); 
+
 if( Meteor.isClient){
     angular
       .module('mexico', ['angular-meteor'])
       .controller('mainCtrl' , mainCtrl);
 
+    mainCtrl.$inject = ['$meteor'];
 
-    function mainCtrl(){
+    function mainCtrl($meteor){
       var main = this;
       main.number= 177618273681726381;
 
-      main.people = [
-        'Josue',
-        'Xochitl',
-        'Israel',
-        'Pablo',
-        'Carlos'
-      ];
+      main.people = $meteor.collection(People);
+        
     }
+}
+
+if( Meteor.isServer){
+  Meteor.startup( function(){
+
+    if( People.find().count() == 0 ){
+      var people = [
+        {'name' : 'Xochitl', 'age': '27'},
+        {'name' : 'Josue', 'age': '29'},
+        {'name' : 'Israel', 'age': '30'},
+      ];
+
+      for(var i = 0 ;i < people.length ; i++){
+        People.insert(people[i]);
+      }
+    }
+
+  });
 }
